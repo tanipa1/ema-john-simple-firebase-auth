57a00b1b55bad820d036989de3409a38a314f1d1
tanipa1 <tania35.291@gmail.com>
2023-04-28 22:29:26 +0600

    Component Login/SignUp

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHOkUjFbc47fiGheaEypOsdSztH5i_ClQ",
+  authDomain: "ema-john-with-firebase-a-68ad3.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-68ad3",
+  storageBucket: "ema-john-with-firebase-a-68ad3.appspot.com",
+  messagingSenderId: "428190360137",
+  appId: "1:428190360137:web:044f2bf1b02324709d5598"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
